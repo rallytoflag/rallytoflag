10a1149c16954b77701aadfbbeff80317bb1fed3
Timothy Marias <timothyjamesmarias@gmail.com>
2023-01-18 13:06:24 -0800

    published email views

--- routes/web.php
+++ routes/web.php
@@ -64,6 +64,7 @@ Route::middleware('auth', 'verified')->group(function () {
   Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
   Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
   Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
   Route::patch('/events/{event}', [EventController::class, 'update'])->name('event.update');
   Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('event.edit');
   Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('event.destroy');
