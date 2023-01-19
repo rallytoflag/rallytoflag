9dbcdfc3b2b791f15e0c917450b4bc5c242fbd43
Timothy Marias <timothyjamesmarias@gmail.com>
2023-01-19 14:09:00 -0800

    fixed index, updated castle placeholder png

--- resources/js/Components/EventCard.vue
+++ resources/js/Components/EventCard.vue
@@ -8,14 +8,14 @@ const props = defineProps({
 </script>
 <template>
 <Link :href="route('event.show', event.id)">
-<div class="rounded-lg bg-neutral-100 dark:bg-neutral-800
+<div class="h-96 w-full rounded-lg bg-neutral-100 dark:bg-neutral-800
 border border-gray-500 hover:border-violet-800 dark:hover:border-violet-300 rounded-lg">
   <img v-if="event.media.length > 0" :src="event.media[0].original_url" alt="event image" 
-  class="xl:lg:md:sm:max-h-[200px] xs:max-h-[100px] object-cover w-full rounded-t-lg" />
+  class="h-1/2 object-cover w-full rounded-t-lg" />
   <img v-else src="castle.png" alt="event image" 
-  class="h-[200px] sm:h-[300px] object-cover w-full rounded-t-lg" />
+  class="h-1/2 object-cover w-full rounded-t-lg" />
 
-  <div class="p-4 min-h-fit max-h-[200px]">
+  <div class="p-4 ">
     <h3 class="text-lg font-bold pb-1">{{ event.title }}</h3>
     <p class="pb-1">{{ event.location }}</p>
     <p class="pb-1"><strong>Starts on </strong>{{ event.start_date }}</p>
